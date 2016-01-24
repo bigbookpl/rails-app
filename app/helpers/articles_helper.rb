3ef5441a833e15eca0c2b4ce14603279766aa835
bigbookpl <bigbook.pl@gmail.com>
2016-01-24 23:45:11 +0100

    Next changes

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,12 @@
 module ArticlesHelper
+  #
+  # # @param [Article] article
+  # def publication_status(article)
+  #   if article.published
+  #     "Published at #{article.created_at.strftime('%A, %B %e')}"
+  #   else
+  #     "Unpublished"
+  #   end
+  # end
+
 end
